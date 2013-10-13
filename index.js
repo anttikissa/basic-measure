ee3b7a4c7aec0e2ddd7be977300fd007b2051aa6
Antti Sykäri <antti.sykari@gmail.com>
2013-10-13 11:28:03 +0300

    More intelligent measuring and reporting.

--- index.js
+++ index.js
@@ -8,15 +8,47 @@ function times(n, f) {
 	return end - start;
 }
 
+// Round number to given precision
+function round(number) {
+	// TODO
+	return String(number);
+}
+
 function measure(f, options) {
-	var time = times(1, f);
+	var totalTime, iterations = 1;
+	do {
+		totalTime = times(iterations, f);
+		iterations *= 2;
+	} while (totalTime < 500);
 
 	var report = {
 		f: f,
 		name: f.name,
-		totalTime: time,
+		totalTime: totalTime,
+		iterations: iterations,
+		time: totalTime / iterations,
+
+		formatTime: function(time) {
+			if (time > 1000) {
+				return round(time / 1000) + ' s';
+			}
+
+			if (time > 1) {
+				return round(time) + ' ms';
+			}
+
+			if (time > 0.001) {
+				return round(time * 1000) + ' μs';
+			}
+
+			if (time > 0.000001) {
+				return round(time * 1000000) + ' ns';
+			}
+			return round(time * 1000000000) + ' ps';
+		},
+
 		toString: function() {
-			return this.name + ": " + this.totalTime + " ms";
+			return this.name + ": " + this.formatTime(this.time);
 		}
 	};
 
